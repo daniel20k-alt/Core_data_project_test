0ae5309b4bb1a93e6ad820b64262bb2d02e0f4c4
Testing_author <testing_beta@mail.com>
2020-08-11 14:06:30 +0300

    Initial Commit

---
+++ Core_data_project_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core_data_project_test
+//
+//  Created by DDDD on 11/08/2020.
+//  Copyright © 2020 MeerkatWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
